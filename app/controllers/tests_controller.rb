11a5297dd5cd669a6e1c20b88ddf7da9540b91fd
intokozo <morozbar@bk.ru>
2019-05-11 17:16:26 +0300

    views

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,4 +1,10 @@
 class TestsController < ApplicationController
+before_action :set_test, only: %i[show edit update destroy]
+
+  def new
+    @test = Test.new
+  end
+
   def index
     @test = Test.all
   end
@@ -6,5 +12,38 @@ class TestsController < ApplicationController
   def show
     test = Test.find(params[:id])
     redirect_to test_questions_path(test.id)
+  end
+
+  def create
+    @test = Test.new(test_params)
+
+    if @test.save
+      redirect_to @test
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @test.update(test_params)
+      redirect_to @test
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @test.destroy
+    redirect_to tests_path
+  end
+
+  private
+
+  def test_params
+    params.require(:test).permit(:title, :level)
+  end
+
+  def set_test
+    @test = Test.find(params[:id])
   end
 end
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,6 @@
 module QuestionsHelper
+  def form_header(question)
+    title = question.new_record? ? 'Создать' : 'Редактировать'
+    "#{title} #{question.test.title} вопрос"
+  end
 end
